b87139fb430d53c58068b6630b04c8c48d10537b
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-08-13 12:14:39 +0530

    Canvas created

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Create the canvas
+
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width=512;
+canvas.height=480;
+document.body.appendChild(canvas);
+console.log("Canvas Created");
