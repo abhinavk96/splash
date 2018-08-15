f75dd4f29ae2ef715d3901501153d80fa0a073ac
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-08-16 02:15:28 +0530

    Make the listening port dynamic

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
 
 });
 app.use('/', express.static(__dirname+'/'));
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log("Server Started");
 var io = require('socket.io')(serv, {});
 var SOCKETS_LIST={};
